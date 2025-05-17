e159a58820cfbe3992f0856cbed29ba8de9b71b4
Luis Felipe <luisbmssi@gmail.com>
2025-05-17 02:00:44 -0300

    Adicionado novos icones

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -3,6 +3,11 @@ import Home from "./home";
 import Plus from "./plus";
 import ThreeDotsHorizontal from "./three-dots-horizontal";
 import { SvgProps } from "react-native-svg";
+import UserIcon from "./user-icon";
+import TrashIcon from "./trash";
+import NewUserIcon from "./new-user";
+import LogoutIcon from "./logout";
+import LoginIcon from "./login";
 
 type IconProps = SvgProps & {
   name: keyof typeof icons
@@ -12,7 +17,12 @@ const icons = {
   home: Home,
   threeDotsHorizontal: ThreeDotsHorizontal,
   plus: Plus,
-  arrowLeft: ArrowLeft
+  arrowLeft: ArrowLeft,
+  userIcon: UserIcon,
+  newUserIcon: NewUserIcon,
+  loginIcon: LoginIcon,
+  logoutIcon: LogoutIcon,
+  trashIcon: TrashIcon
 }
 
 export default function Icon({ name, ...props }: IconProps) {
